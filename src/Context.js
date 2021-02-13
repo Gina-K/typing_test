c7cb9bb2bed0808bcd13880024eb331e327af84f
Eugenia Kurmeneva <e.kurmeneva@gmail.com>
2021-02-13 14:19:03 +0300

    Replace hardcoded sample text with Context

--- src/Context.js
+++ src/Context.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 const Context = React.createContext();
 
+const boilerplateText = "The API is not responding, so you can only type this example of a text. A wonderful serenity " +
+    "has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart." +
+    " I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. " +
+    "I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my " +
+    "talents. I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was " +
+    "a greater artist than now."
+
 function ContextProvider({children}) {
+    const [textForTyping, setTextForTyping] = useState(boilerplateText);
+
     return (
-        <Context.Provider value={{}}>
+        <Context.Provider value={{
+            textForTyping,
+        }}>
             {children}
         </Context.Provider>
     );
--- src/components/SampleTextArea.js
+++ src/components/SampleTextArea.js
@@ -1,22 +1,20 @@
-import React from "react";
+import React, {useContext} from "react";
 import {
     Card,
     CardBody,
     CardText
 } from "reactstrap";
 
+import {Context} from "../Context";
+
 function SampleTextArea() {
+    const {textForTyping} = useContext(Context);
+
     return (
         <Card>
             <CardBody>
                 <CardText>
-                    Some text from lorem ipsum API will be here. Bacon ipsum dolor amet shoulder sint duis, eiusmod
-                    minim pork loin dolore flank. Hamburger nulla biltong shoulder picanha aute qui swine salami
-                    bresaola laborum. Qui excepteur lorem cupidatat ut venison filet mignon officia landjaeger fugiat.
-                    Elit do culpa sint ut, doner beef ribs. Beef corned beef sirloin dolore porchetta pariatur pork loin
-                    andouille nostrud jowl tail bresaola kevin. Fugiat eiusmod fatback venison ullamco consequat bacon
-                    pork loin. Sirloin deserunt turducken pancetta, dolore chuck shank voluptate jerky tri-tip velit
-                    reprehenderit lorem strip steak laborum.
+                    {textForTyping}
                 </CardText>
             </CardBody>
         </Card>
